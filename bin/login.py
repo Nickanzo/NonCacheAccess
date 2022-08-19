0291943e2e0b0e8c9607d27b8bfadb7ec4b338f7
Nickanzo <nicoeanzola@gmail.com>
2022-08-19 04:22:03 -0300

    Adicionado cabeçalho, código melhor comentado e aprimoramento na formatação.

--- bin/login.py
+++ bin/login.py
@@ -1,14 +1,29 @@
-from dbCon import *
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Nicolas Escobar
+# Created Date: 17/08/2022
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Login screen """
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+from dbCon import create_con, verify_user
 from settings import *
 
+
+# Login method for account management display
 def login(username, password):
+    # Create DB connection using settings.py definitions
     con = create_con(HOST, USER, PASSWORD, NAME)
 
-    if(con.is_connected()):
-        if(verify_user(con, username, password)):
-            print("Login com sucesso")
-        else:
-            print("Erro Usuário ou Senha")
+    #Verify if the connection succeeded
+    if not con.is_connected():
+        print("DB connection error!")
     else:
-        print("Erro na conexão com o Banco")
-
+        #Search the username/password in the DB
+        if verify_user(con, username):
+            print("Success!")
+        else:
+            print("Username or Password incorrect!")
