289e87a6b7cadd9c02eb3b768669491c67063ee3
Nickanzo <nicoeanzola@gmail.com>
2022-08-19 04:16:49 -0300

    Adicionado cabeçalho no arquivo de configurações

--- settings.py
+++ settings.py
@@ -1,4 +1,14 @@
-#Database config
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Nicolas Escobar
+# Created Date: 17/08/2022
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Settings file for general data used along the app"""
+# ---------------------------------------------------------------------------
+# Database Config
+# ---------------------------------------------------------------------------
 # HOST = 'sql10.freesqldatabase.com'
 HOST = 'localhost'
 # NAME = 'sql10513493'
@@ -8,3 +18,6 @@ USER = 'admin'
 # PASSWORD = 'DXDKiyNZB8'
 PASSWORD = 'admin'
 PORT = '3306'
+
+
+
